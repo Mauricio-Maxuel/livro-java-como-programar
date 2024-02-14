fd580a200e1ef0ac8c3c04c979706db101576eae
mauricio <mauricio.msantos@sptech.school>
2024-02-14 12:56:28 -0300

    init commit

---
+++ src/main/java/org/maumas/CCapitulo4/whileExamples/doWhile.java
@@ -0,0 +1,15 @@
+package org.maumas.CCapitulo4.whileExamples;
+
+public class doWhile {
+
+    public static void main(String[] args) {
+
+        int i = 0;
+
+        do {
+            System.out.println("My number is " + i);
+            i++;
+        } while (i < 10);
+    }
+
+}
