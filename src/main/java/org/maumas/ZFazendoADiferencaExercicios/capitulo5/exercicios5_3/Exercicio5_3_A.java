02928b7dcdd9589052753a8f6202e10f8f281c49
mauricio <mauricio.msantos@sptech.school>
2024-02-15 14:26:55 -0300

    feat: exercicios capitulo 5

---
+++ src/main/java/org/maumas/ZFazendoADiferencaExercicios/capitulo5/exercicios5_3/Exercicio5_3_A.java
@@ -0,0 +1,16 @@
+package org.maumas.ZFazendoADiferencaExercicios.capitulo5.exercicios5_3;
+
+public class Exercicio5_3_A {
+
+    public static void main(String[] args) {
+
+        int sum = 0;
+
+        for (int count = 1; count <= 99; count += 2) {
+            sum += count;
+        }
+
+        System.out.println("soma total" + sum);
+    }
+
+}
