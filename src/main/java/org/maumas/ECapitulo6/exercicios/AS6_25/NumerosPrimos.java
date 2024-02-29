fb30c627fed7dc226b561418a7ec592ac7f19a12
mauricio <mauricio.msantos@sptech.school>
2024-02-29 11:44:33 -0300

    feat: adicionando alguns exercicios

---
+++ src/main/java/org/maumas/ECapitulo6/exercicios/AS6_25/NumerosPrimos.java
@@ -0,0 +1,35 @@
+package org.maumas.ECapitulo6.exercicios.AS6_25;
+
+public class NumerosPrimos {
+
+    public static int numbers = 0;
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 1000; i++) {
+            if (isPrimo(i)) {
+                System.out.println(i);
+            }
+        }
+
+        System.out.println("qtd verificacoes " + numbers);
+    }
+
+    public static boolean isPrimo(int number) {
+
+        int times = 0;
+        for (int i = 1; i <= Math.sqrt(number); i++) {
+            if (times == 3) {
+                break;
+            }
+
+            if (number % i == 0) {
+                times++;
+            }
+        }
+
+
+        return !(number == 0 || number == 1) && times != 2;
+    }
+
+}
