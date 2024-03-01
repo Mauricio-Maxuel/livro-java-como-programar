21979be7d9ad524ffbfb15a8d3b599ff5b8b9fb6
mauricio <mauricio.msantos@sptech.school>
2024-03-01 12:22:43 -0300

    feat: exercicios regulares capitulo 6

---
+++ src/main/java/org/maumas/ECapitulo6/exercicios/AX6_30/AdivinheNumero.java
@@ -0,0 +1,48 @@
+package org.maumas.ECapitulo6.exercicios.AX6_30;
+
+import java.security.SecureRandom;
+import java.util.Scanner;
+
+public class AdivinheNumero {
+
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+
+        int escolha = 1;
+
+        while (escolha != 0) {
+
+            System.out.println("vamos começar!");
+
+            gaming();
+
+            System.out.println("Deseja tentar novamente?");
+            System.out.println("0 -> não | 1 -> sim");
+            escolha = scanner.nextInt();
+        }
+
+    }
+
+    public static void gaming() {
+        System.out.println("Tente adivinhar o número");
+        SecureRandom secureRandom = new SecureRandom();
+
+        int number = secureRandom.nextInt(1000);
+        int chose;
+        do {
+            chose = scanner.nextInt();
+
+            if (chose > number) {
+                System.out.println("Muito Alto, tente novamente");
+            } else if (chose < number) {
+                System.out.println("Muito Baixo, tente novamente");
+            }
+
+        } while (chose != number);
+        System.out.println("Parabéns, você adivinhou o número!!!");
+    }
+
+
+}
---
+++ src/main/java/org/maumas/ECapitulo6/exercicios/AY6_31/AdivinheAModificaoNumero.java
@@ -0,0 +1,63 @@
+package org.maumas.ECapitulo6.exercicios.AY6_31;
+
+import org.w3c.dom.ls.LSOutput;
+
+import java.security.SecureRandom;
+import java.util.Scanner;
+
+public class AdivinheAModificaoNumero {
+
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+
+        int escolha = 1;
+
+        while (escolha != 0) {
+
+            System.out.println("vamos começar!");
+
+            gaming();
+
+            System.out.println("Deseja tentar novamente?");
+            System.out.println("0 -> não | 1 -> sim");
+            escolha = scanner.nextInt();
+        }
+
+    }
+
+    public static void gaming() {
+        int tentativas = 0;
+
+        System.out.println("Tente adivinhar o número");
+        SecureRandom secureRandom = new SecureRandom();
+
+        int number = secureRandom.nextInt(1000);
+        int chose;
+        do {
+            chose = scanner.nextInt();
+
+            if (chose > number) {
+                System.out.println("Muito Alto, tente novamente");
+            } else if (chose < number) {
+                System.out.println("Muito Baixo, tente novamente");
+            }
+            tentativas++;
+        } while (chose != number);
+
+        exibeResultado(tentativas);
+    }
+
+
+    public static void exibeResultado(int tentativas) {
+        if (tentativas > 10) {
+            System.out.println("Você deve ser capaz de fazer melhor");
+        } else if (tentativas == 10) {
+            System.out.println("Aha! Você sabe o segredo!");
+        } else {
+            System.out.println("Você sabe o segredo ou tem muita sorte!");
+        }
+    }
+
+}
